0723453ee5e636e53160b11343ee11c664dfcf5b
diannehidalgo117 <156630491+diannehidalgo117@users.noreply.github.com>
2024-03-14 15:04:29 +0900

    rprename.py

---
+++ rprename.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# rprenamer.py
+
+"""このモジュールは名前の変更ツールのentry-point scriptを提供する。"""
+
+from rprename.app import main
+
+if __name__ == "__main__":
+    main()
