9147c8c7c46cf792dc004deb6d4cb9d1eb05f0b4
diannehidalgo117 <156630491+diannehidalgo117@users.noreply.github.com>
2024-03-14 16:30:12 +0900

    Add files via upload

---
+++ rprename/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# rprename/__init__.py
+
+"""このモジュールはrprenameパッケージを提供する。"""
+
+__version__ = "0.1.0"
---
+++ rprename/app.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""このモジュールは名前の変更アプリケーションを提供する。"""
+
+import sys
+
+from PyQt5.QtWidgets import QApplication
+
+from .views import Window
+
+def main():
+    # Create the application
+    app = QApplication(sys.argv)
+    # Create and show the main window
+    win = Window()
+    win.show()
+    # Run the event loop
+    sys.exit(app.exec())
